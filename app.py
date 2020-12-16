2dd81a192825d51a4c0663031d01312e3097ed75
yuche <cheyuhnu@gmail.com>
2020-12-16 00:18:36 +0000

    Update for mini app

--- app.py
+++ app.py
@@ -6,20 +6,21 @@ Author: Yu Che
 """
 
 import pandas as pd
-import plotly.graph_objects as go
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 
-from utils import df_selection, structure_viewer
+from utils import th4_plot, structure_viewer
 from config import CONFIG
 
+# Set up web server
 app = dash.Dash(__name__)
 server = app.server
+# Load dataframe
+df = pd.read_csv('./data/th4.csv')
+columns_dict = [{'label': i, 'value': i} for i in df.columns[1:-1]]
 
-# Load DataFrames
-df = pd.read_csv()
-
+# Application object and HTML components
 app.layout = html.Div(
     id='main',
     className='app_main',
@@ -27,7 +28,14 @@ app.layout = html.Div(
         # Dash title and icon
         html.Div(
             id='mol3d-title',
-            children=[]
+            children=[
+                html.Img(
+                    id='dash-logo',
+                    src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/"
+                        "logo/new-branding/dash-logo-by-plotly-stripe.png"
+                ),
+                html.H1(id='chart-title', children='ESF maps')
+            ]
         ),
         # Dash graph and 3D molecule viewer
         dcc.Graph(id='indicator-graphic', config=CONFIG),
@@ -38,23 +46,67 @@ app.layout = html.Div(
                 html.H3('Chart types:'),
                 dcc.RadioItems(
                     id='chart-type',
-                    options=[]
+                    options=[
+                        {'label': 'ESF Map: TH4', 'value': 'th4'}
+                    ],
+                    value='th4'
                 ),
                 html.Div(
                     id='color-bar-control',
                     className='dropdown-control',
-                    children=[]
+                    children=[
+                        html.H3('Colour bar:'),
+                        dcc.Dropdown(
+                            id='colour_column',
+                            options=columns_dict,
+                            value='No. of hydrogen bonds'
+                        )
+                    ]
                 ),
                 html.Div(
                     id='x-axis',
                     className='dropdown-control',
-                    children=[]
+                    children=[
+                        html.H3('X-axis:'),
+                        dcc.Dropdown(
+                            id='x_axis_column',
+                            className='axis_controller',
+                            options=columns_dict,
+                            value='Density (g/cm^3)'
+                        )
+                    ]
                 ),
                 html.Div(
                     id='y-axis',
                     className='dropdown-control',
-                    children=[]
-                )
+                    children=[
+                        html.H3('Y-axis:'),
+                        dcc.Dropdown(
+                            id='y_axis_column',
+                            className='axis_controller',
+                            options=columns_dict,
+                            value='Relative lattice energy (kJ/mol)'
+                        )
+                    ]
+                ),
+                html.P('The X and Y axis dropdown are disabled '
+                       'when choose landmarks chart.'),
+                html.Div(
+                    id='range-slider-control',
+                    className='dropdown-control',
+                    children=[
+                        html.H3('Range slider:'),
+                        dcc.Dropdown(
+                            id='range_column',
+                            options=columns_dict,
+                            value='Relative lattice energy (kJ/mol)'
+                        )
+                    ]
+                ),
+                # Dash range slider and texts
+                dcc.RangeSlider(id='range-slider'),
+                html.P(id='selected_data'),
+                # User instructions
             ]
         ),
         html.Div(
@@ -69,43 +121,76 @@ app.layout = html.Div(
 )
 
 
+# Setting range slider properties
+@app.callback(
+    dash.dependencies.Output('range-slider', 'min'),
+    [dash.dependencies.Input('range_column', 'value')])
+def select_bar1(range_column_value):
+    return df[range_column_value].min()
+
+
+@app.callback(
+    dash.dependencies.Output('range-slider', 'max'),
+    [dash.dependencies.Input('range_column', 'value')])
+def select_bar2(range_column_value):
+    return df[range_column_value].max()
+
+
+@app.callback(
+    dash.dependencies.Output('range-slider', 'value'),
+    [dash.dependencies.Input('range_column', 'value')])
+def select_bar3(range_column_value):
+    return [df[range_column_value].min(), df[range_column_value].max()]
+
+
+@app.callback(
+    dash.dependencies.Output('range-slider', 'step'),
+    [dash.dependencies.Input('range_column', 'value')]
+)
+def range_step(range_column_value):
+    step = (df[range_column_value].max() - df[range_column_value].min())/100
+    return step
+
+
+# Print the selected range
+@app.callback(
+    dash.dependencies.Output('selected_data', 'children'),
+    [dash.dependencies.Input('range-slider', 'value'),
+     dash.dependencies.Input('range_column', 'value')])
+def callback(range_slider_value, range_column_value):
+    return 'Structure {} between {:>.1f} and {:>.1f} are selected.'.format(
+        range_column_value, range_slider_value[0], range_slider_value[1]
+    )
+
+
 # Chemical structure 3D viewer
 @app.callback(
     dash.dependencies.Output('loading_selected', 'children'),
-    [dash.dependencies.Input('indicator-graphic', 'selectedData'),
-     dash.dependencies.Input('colour_column', 'value'),
-     dash.dependencies.Input('chart-type', 'value')])
-def display_selected_structure(selectedData, colour_column_value,
-                               chart_type_value):
-    return structure_viewer(selectedData, colour_column_value, chart_type_value)
+    [dash.dependencies.Input('indicator-graphic', 'selectedData')])
+def display_selected_structure(selectedData):
+    return structure_viewer(df, selectedData)
 
 
 # Figure updated by different dash components
 @app.callback(
     dash.dependencies.Output('indicator-graphic', 'figure'),
-    [dash.dependencies.Input('chart-type', 'value'),
-     dash.dependencies.Input('x_axis_column', 'value'),
+    [dash.dependencies.Input('x_axis_column', 'value'),
      dash.dependencies.Input('y_axis_column', 'value'),
-     dash.dependencies.Input('colour_column', 'value')])
-def update_graph(chart_type_value, x_axis_column_name, y_axis_column_name,
-                 colour_column_value):
+     dash.dependencies.Input('colour_column', 'value'),
+     dash.dependencies.Input('range_column', 'value'),
+     dash.dependencies.Input('range-slider', 'value')])
+def update_graph(x_axis_column_name, y_axis_column_name, colour_column_value,
+                 range_column_value, range_slider_value):
+    filtered_df = pd.DataFrame(
+        data=df[
+            (df[range_column_value] >= range_slider_value[0]) &
+            (df[range_column_value] <= range_slider_value[1])]
+    )
     # General ESF map
-    df_selected = df_selection(chart_type_value)
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(
-        x=df_selected[x_axis_column_name],
-        y=df_selected[y_axis_column_name],
-        text=df_selected[''],
-        mode='markers',
-        marker={'size': 10,
-                'color': df_selected[colour_column_value],
-                'colorbar': {'title': colour_column_value},
-                'colorscale': 'RdBu',
-                'showscale': True}
-        ))
-    fig.update_layout()
+    fig = th4_plot(filtered_df, x_axis_column_name, y_axis_column_name,
+                   colour_column_value)
     return fig
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-These functions is designed for crystal structure formation and the 3D molecular
-viewer for the application
+These functions is designed for crystal structure formation and
+the 3D molecular viewer application
 """
 import json
 import pandas as pd
+
+import plotly.graph_objects as go
 import dash_html_components as html
 import dash_bio as bio
 
@@ -13,9 +15,11 @@ import dash_bio as bio
 def load_json(file_path):
     """
     Loading the structure json file
+
     Parameters
     ----------
     file_path: str
+
     Returns
     -------
 
@@ -25,40 +29,57 @@ def load_json(file_path):
     return mol_data, style_data
 
 
-def df_selection(chart_name):
-    """
+def th4_plot(df, x_axis_column_name, y_axis_column_name, colour_column_value):
+    fig = go.Figure()
+    # The rest figure have a continues color bar.
+    fig.add_trace(go.Scatter(
+        x=df[x_axis_column_name],
+        y=df[y_axis_column_name],
+        text=df.index,
+        marker=dict(
+            color=df[colour_column_value],
+            colorscale='RdBu',
+            colorbar=dict(
+                thicknessmode='pixels',
+                thickness=20,
+                title=dict(text=colour_column_value, side='right')
+            ),
+            reversescale=True,
+            showscale=True
+        )
+    ))
+    # Set the format of points
+    fig.update_traces(
+        mode='markers',
+        marker=dict(
+            symbol='circle',
+            opacity=0.7,
+            line=dict(color='rgb(40, 40, 40)', width=0.2),
+            size=8
+        )
+    )
+    # Set the format of axes
+    axis_template = dict(linecolor='#444', tickcolor='#444',
+                         ticks='outside', showline=True, zeroline=False,
+                         gridcolor='lightgray')
+    fig.update_layout(
+        xaxis=dict(axis_template, **dict(title=x_axis_column_name)),
+        yaxis=dict(axis_template, **dict(title=y_axis_column_name)),
+        clickmode='event+select',
+        hovermode='closest',
+        plot_bgcolor='white'
+    )
+    return fig
 
-    Parameters
-    ----------
-    chart_name
 
-    Returns
-    -------
-
-    """
-    # Load DataFrames
-    df_trip = pd.read_csv('./data/df_trip.csv')
-    energy_trip = pd.read_csv('./data/energy_trip.csv')
-    th4 = pd.read_csv('./data/th4.csv')
-    if chart_name == 'th4':
-        return th4
-    elif chart_name == 'df_trip':
-        return df_trip
-    elif chart_name == 'energy_trip':
-        return energy_trip
-    else:
-        raise ValueError('Chart type error')
-
-
-def structure_viewer(interactive_data, color_bar, chart_name):
+def structure_viewer(df, interactive_data):
     """
     The molecular viewer
 
     Parameters
     ----------
+    df: Dataframe
     interactive_data: dict
-    color_bar: str
-    chart_name: str
 
     Returns
     -------
@@ -83,33 +104,12 @@ def structure_viewer(interactive_data, color_bar, chart_name):
 
     mol_div = []
     # Loading multiple 3D viewer
-    cluster_dict = {'0': 'TH4', '1': 'TH2', '2': 'T2'}
     try:
         for i in range(len(interactive_data['points'])):
-            df = df_selection(chart_name=chart_name)
-            if chart_name == 'th4':
-                if color_bar == 'Pore dimensionality':
-                    index = int(interactive_data['points'][i]['pointIndex'])
-                    cluster_idx = interactive_data['points'][i]['curveNumber']
-                    selected_df = df[
-                        df.loc[:, 'Pore dimensionality'] == int(cluster_idx)
-                        ].iloc[index]
-                    structure_name = selected_df.Name
-                    origin_idx = selected_df.index[0]
-
-                else:
-                    index = int(interactive_data['points'][i]['pointIndex'])
-                    structure_name = int(df.iloc[index].Name)
-                    origin_idx = index
-            else:
-                # Finding the original index from the grouped data
-                index = int(interactive_data['points'][i]['pointIndex'])
-                cluster_idx = interactive_data['points'][i]['curveNumber']
-                cluster_name = cluster_dict[str(cluster_idx)]
-                selected_df = df[df.Molecule == cluster_name].iloc[index]
-                structure_name = selected_df.Name
-                origin_idx = selected_df.name
-            json_path = './data/{}/{}.json'.format(chart_name, structure_name)
+            index = int(interactive_data['points'][i]['pointIndex'])
+            structure_name = int(df.iloc[index].Name)
+            origin_idx = index
+            json_path = './data/th4/{}.json'.format(structure_name)
             viewer = single_3d_viewer(json_path, origin_idx)
             mol_div.append(viewer)
     # Default structure
